42b764536cb1e8e49a2e2b5120e5de88c139c5c6
Hiroyuki Furihata <furihatah@gmail.com>
2015-11-21 11:04:37 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -sf ~/dotfiles/zshrc ~/.zshrc
+ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/vim ~/.vim
