62f81423c35c1f0af05ec0bea883305f447b1b4a
Hiroyuki Furihata <furihatah@gmail.com>
2015-11-29 21:29:40 +0900

    Updated

--- setup.sh
+++ setup.sh
@@ -4,5 +4,9 @@ ln -sf $(pwd)/zshrc ${HOME}/.zshrc
 ln -sf $(pwd)/vimrc ${HOME}/.vimrc
 ln -sf $(pwd)/gvimrc ${HOME}/.gvimrc
 ln -sf $(pwd)/tmux.conf ${HOME}/.tmux.conf
-ln -sf $(pwd)/vim ${HOME}/.vim
-ln -sf $(pwd)/localsettings ${HOME}/.localsettings
+if [ ! -e ${HOME}/.vim ]; then
+  ln -sf $(pwd)/vim ${HOME}/.vim
+fi
+if [ ! -e ${HOME}/.localsettings ]; then
+  ln -sf $(pwd)/localsettings ${HOME}/.localsettings
+fi
