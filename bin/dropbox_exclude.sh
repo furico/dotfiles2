d77d65015a8fc4a5472905fd9ddbd4108a31cba6
Hiroyuki Furihata <furihatah@gmail.com>
2015-12-05 18:27:45 +0900

    Updated dropbox_exclude.sh

--- bin/dropbox_exclude.sh
+++ bin/dropbox_exclude.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Dropboxyから同期するディレクトリを除外するスクリプト
-DROPBOX_PY="${HOME}/Dropbox/dropbox.py"
+
+DROPBOX_DIR="${HOME}/Dropbox"
+DROPBOX_PY="${DROPBOX_DIR}/dropbox.py"
+
 if [ ! -f ${DROPBOX_PY} ]; then
   echo "not found ${DROPBOX_PY}"
   exit 1
 fi
 
-paths="${HOME}/*"
-for path in ${paths}; do
-  if [ -d $path ]; then
-    echo $path
+db_paths="${DROPBOX_DIR}/*"
+for db_path in ${db_paths}; do
+  if [ -d ${db_path} ]; then
+    python ${DROPBOX_PY} exclude add ${db_path}
   fi
 done
