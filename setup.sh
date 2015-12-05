3388914e7e1d8d06d797552c78d944306dc09ea1
Hiroyuki Furihata <furihatah@gmail.com>
2015-12-05 10:04:03 +0900

    binディレクトリを追加

--- setup.sh
+++ setup.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+#
+# ホームディレクトリにシンボリックリンクを作成する
+#
+
+# ファイル
 ln -sf $(pwd)/zshrc ${HOME}/.zshrc
 ln -sf $(pwd)/vimrc ${HOME}/.vimrc
 ln -sf $(pwd)/gvimrc ${HOME}/.gvimrc
 ln -sf $(pwd)/tmux.conf ${HOME}/.tmux.conf
+
+# ディレクトリ
 if [ ! -e ${HOME}/.vim ]; then
   ln -sf $(pwd)/vim ${HOME}/.vim
 fi
 if [ ! -e ${HOME}/.localsettings ]; then
   ln -sf $(pwd)/localsettings ${HOME}/.localsettings
 fi
+ln -sf $(pwd)/bin ${HOME}
+
+# Dropbox
 if [ -e ${HOME}/Dropbox/riv_vim ]; then
   ln -sf ${HOME}/Dropbox/riv_vim ${HOME}
 fi
